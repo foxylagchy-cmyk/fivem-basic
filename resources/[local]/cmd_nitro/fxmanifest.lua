316128dea44ac0c0dd168da3ed31366d2d178aac
Gustyx-Power <gustiadityamuzaky08@gmail.com>
2026-06-22 01:58:42 +0700

    feat(cmd_nitro): add nitro boost command resource with flame effects

---
+++ resources/[local]/cmd_nitro/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'cmd_nitro'
+description 'Memberikan NOS ke kendaraan secara instan'
+author 'GustyxPower'
+
+client_script 'client.lua'
+server_script 'server.lua'
